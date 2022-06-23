a893e1945ab5ba01c737791ef71df07f491c40aa
lehungson0510 <hungsonle0510@gmail.com>
2022-06-24 01:49:43 +0700

    up

--- ss2_spring_controller/bai_tap/may_tinh_ca_nhan/src/main/java/codegym/vn/controller/CalculatorController.java
+++ ss2_spring_controller/bai_tap/may_tinh_ca_nhan/src/main/java/codegym/vn/controller/CalculatorController.java
@@ -17,29 +17,34 @@ public class CalculatorController {
 
     @PostMapping("/calculator")
     public String home(Model model, @RequestParam("calculation") String calculation, @RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        double result = 0;
         switch (calculation) {
             case "+":
-                model.addAttribute("result", num1 + num2);
+                result = num1 + num2;
                 model.addAttribute("calculation", "Result Addition: ");
                 break;
             case "-":
-                model.addAttribute("result", num1 - num2);
+                result = num1 - num2;
                 model.addAttribute("calculation", "Result Subtraction: ");
                 break;
             case "*":
-                model.addAttribute("result", num1 * num2);
+                result = num1 * num2;
                 model.addAttribute("calculation", "Result Multiplication: ");
                 break;
             case "/":
                 if (num2 == 0) {
                     model.addAttribute("result", "Result Division: error");
                     model.addAttribute("message", "Invalid");
+                    model.addAttribute("number1", num1);
+                    model.addAttribute("number2", num2);
+                    return "home";
                 } else {
-                    model.addAttribute("result", num1 / num2);
+                    result = num1 / num2;
                     model.addAttribute("calculation", "Result Multiplication:");
                 }
                 break;
         }
+        model.addAttribute("result", result);
         model.addAttribute("number1", num1);
         model.addAttribute("number2", num2);
         return "home";
--- ss2_spring_controller/bai_tap/may_tinh_ca_nhan/src/main/webapp/WEB-INF/views/home.jsp
+++ ss2_spring_controller/bai_tap/may_tinh_ca_nhan/src/main/webapp/WEB-INF/views/home.jsp
@@ -15,8 +15,8 @@
 <h1>Calculator</h1>
 <form action="/calculator" method="post">
     <div style="margin-bottom: 10px">
-        <input  required placeholder="${number1}" type="text" name="num1">
-        <input  required placeholder="${number2}" type="text" name="num2">
+        <input required placeholder="${number1}" type="text" name="num1">
+        <input required placeholder="${number2}" type="text" name="num2">
     </div>
     <button name="calculation" type="submit" value="+">Addition(+)</button>
     <button name="calculation" type="submit" value="-">Subtraction(-)</button>
@@ -26,5 +26,8 @@
 <%--<c:if test="${result != }"></c:if>--%>
 <h3 style="color: red">${message}</h3>
 <h3>${calculation} ${result}</h3>
+<%--<c:if test="${!message.equals('Invalid')}">--%>
+<%--    <h3>${calculation} ${result}</h3>--%>
+<%--</c:if>--%>
 </body>
 </html>
