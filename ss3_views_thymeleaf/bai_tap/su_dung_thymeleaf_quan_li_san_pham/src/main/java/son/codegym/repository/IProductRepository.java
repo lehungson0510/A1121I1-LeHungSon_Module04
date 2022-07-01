01014452011285a452b3f89d5a5bb9b8eeb194f6
lehungson0510 <hungsonle0510@gmail.com>
2022-07-02 04:17:38 +0700

    up

---
+++ ss3_views_thymeleaf/bai_tap/su_dung_thymeleaf_quan_li_san_pham/src/main/java/son/codegym/repository/IProductRepository.java
@@ -0,0 +1,19 @@
+package son.codegym.repository;
+
+import son.codegym.entity.Product;
+
+import java.util.List;
+
+public interface IProductRepository {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product customer);
+
+    void remove(int id);
+
+    List<Product> search(String name);
+}
---
+++ ss3_views_thymeleaf/bai_tap/su_dung_thymeleaf_quan_li_san_pham/src/main/java/son/codegym/service/IProductService.java
@@ -0,0 +1,19 @@
+package son.codegym.service;
+
+import son.codegym.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product customer);
+
+    void remove(int id);
+
+    List<Product> search(String name);
+}
